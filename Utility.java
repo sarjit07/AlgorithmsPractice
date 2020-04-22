6e7ff91771118bd06e8fdb8226662c45665747c1
Arjit Sharma <sharma.arjit07@gmail.com>
2020-04-22 13:12:34 +0530

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,7 +1,7 @@
 public class Utility {
-	
-	long MOD = (long)1e9 + 7;
-	
+
+	long MOD = (long) 1e9 + 7;
+
 	public static long ModPow(long x, long y, long MOD) {
 		long res = 1L;
 		x = x % MOD;
@@ -23,6 +23,23 @@ public class Utility {
 		} else {
 			return (x % MOD) * modPowRecur(x, y - 1, MOD);
 		}
+	}
+
+	public static long gcdExtended(long a, long b, long x, long y) {
+		if (a == 0) {
+			x = 0;
+			y = 1;
+			return b;
+		}
+
+		long x1 = 1, y1 = 1; // To store results of recursive call
+		long gcd = gcdExtended(b % a, a, x1, y1);
+
+		// Update x and y using results of recursive call
+		x = y1 - (b / a) * x1;
+		y = x1;
+
+		return gcd;
 	}
 
 }
