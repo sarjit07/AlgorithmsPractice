c8ca551f63798f8947b3efde4928719d42c2efaf
Arjit Sharma <sharma.arjit07@gmail.com>
2021-04-12 16:16:35 +0530

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -160,24 +160,24 @@ public class Utility {
 	}
 
 	// returns => x^y % MOD
-	public static long ModPow(long x, long y, long MOD) {
+	public static long ModPowRecur(long x, long y, long MOD) {
 		long res = 1L;
-		x = x % MOD;
+		
 		while (y >= 1) {
 			if ((y & 1) > 0)
-				res = (res * x) % MOD;
-			x = (x * x) % MOD;
+				res = (res%MOD * x%MOD) % MOD;
+			x = (x%MOD * x%MOD) % MOD;
 			y >>= 1;
 		}
 		return res;
 	}
 
-	public static long modPowRecur(long x, long y, long MOD) {
+	public static long modPow(long x, long y, long MOD) {
 		if (y == 0)
 			return 1;
 		else if (y % 2 == 0) {
 			long z = modPowRecur(x, y / 2, MOD);
-			return (z * z) % MOD;
+			return (z%MOD * z%MOD) % MOD;
 		} else {
 			return (x % MOD) * modPowRecur(x, y - 1, MOD);
 		}
